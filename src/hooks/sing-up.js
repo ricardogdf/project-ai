e5f460721f42e4ba0d35e6abe551da35da6eded8
ricardogdf <ricardogf2004@gmail.com>
2024-07-01 09:36:46 -0300

    Retornando de maneira correta ao cadastrar

--- src/hooks/sing-up.js
+++ src/hooks/sing-up.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const handleSingUp = async (data) => {
 
   try {
-    const response = await axios.post(
+    return await axios.post(
       'http://10.240.20.176:8080/DataMask/webresources/api/Usuario/Inserir', 
       {
         nomecliente: data.name,
@@ -17,8 +17,6 @@ export const handleSingUp = async (data) => {
         }
       }
     );
-    console.log('Dados enviados com sucesso:', response.data);
-    return response.data
   } catch (error) {
     console.error('Erro ao fazer a requisição:', error);
   }
